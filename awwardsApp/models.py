70a49cc095f9ff992392fa5e510342b393bb7860
derrickmacharia <derrick.macharia@student.moringaschool.com>
2021-12-09 21:43:41 +0300

    add and style navbar

--- awwardsApp/models.py
+++ awwardsApp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+import datetime as dt
+from django.contrib.auth.models import User
+from django.db.models.fields import related
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
+
+class Location(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Image(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='images')
+    image = CloudinaryField('image')
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,null=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE,null=True)
+    post_date = models.DateTimeField(auto_now_add=True,null=True)
