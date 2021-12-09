ae418750869896a866dab2fab422131590f94764
derrickmacharia <derrick.macharia@student.moringaschool.com>
2021-12-09 23:02:10 +0300

    intergrate models to views

--- awwardsApp/views.py
+++ awwardsApp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from awwardsApp.models import *
 
 # Create your views here.
+
+@login_required(login_url='/accounts/login/')
 def home(request):
-    return render(request, 'home.html')
+    images = Image.objects.all()
+
+    return render(request, 'home.html',{'images': images})
