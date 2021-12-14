8d63fc3ea3906a6825ae5052f93ab22c4f7bb896
derrickmacharia <derrick.macharia@student.moringaschool.com>
2021-12-15 00:07:25 +0300

    test project class

--- awwardsApp/tests.py
+++ awwardsApp/tests.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
-from .models import  Profile
+from .models import  Profile, Project
 
 
 class ProfileTestClass(TestCase):
@@ -39,3 +39,36 @@ class ProfileTestClass(TestCase):
         self.profile.create_profile()
         profiles = Profile.objects.all()
         self.assertTrue(len(profiles) > 0)
+
+class ProjectTestClass(TestCase):
+    def setUp(self):
+        user = User.objects.create(
+            username="test_user", first_name="ezekiel", last_name="kibiego"
+        )
+
+        self.project = Project(
+            title="Test Photos",
+            description="Test Category",
+            image="image.png",
+            user=user,
+        )
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.project, Project))
+
+    def test_save_method(self):
+        self.project.save_project()
+        projects = Project.objects.all()
+        self.assertTrue(len(projects) > 0)
+    
+
+    def test_update_project(self):
+        self.project.save_project()
+        projects = Project.objects.all()
+        self.assertTrue(len(projects) > 0)
+
+    def test_delete_method(self):
+        self.project.save_project()
+        self.project.delete_project()
+        projects = Project.objects.all()
+        self.assertTrue(len(projects) == 0)
